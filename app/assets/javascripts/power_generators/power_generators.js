1d5e8a5176d414b19cfd02c686610ee9a76b0356
gilcierweb <gilcier06@yahoo.com.br>
2019-10-07 17:02:57 -0300

    adding bootstrap cdn and improve grid products

---
+++ app/assets/javascripts/power_generators/power_generators.js
@@ -0,0 +1,14 @@
+function ready(event) {
+  $('input[type=radio][name=searchRadio]').change(function() {
+    if (this.value === 'simple') {
+      $('.simple-form').show();
+      $('.advanced-form').hide();
+    }
+    else if (this.value === 'advanced') {
+      $('.simple-form').hide();
+      $('.advanced-form').show();
+    }
+  });
+}
+
+$(document).on('turbolinks:load', ready);
